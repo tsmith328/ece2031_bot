83a0c3fb54785e0c1ba8a960a3216df81dafe1e9
Tyler Smith <tsmith328@gatech.edu>
2015-11-16 14:02:50 -0500

    Sorting script done.

--- sort.py
+++ sort.py
@@ -1,68 +1,76 @@
-f = open("points.txt", "r")
+def sort():
+    f = open("points.txt", "r")
 
-points = []
+    points = []
 
-i = 1
+    i = 1
 
-#Read in the points
-for line in f:
-	line = line.rstrip('\n')
-	line = line.split(',')
-	points.append((i, line[0], line[1]))
-	i += 1
+    #Read in the points
+    for line in f:
+        line = line.rstrip('\n')
+        line = line.split(',')
+        points.append((i, line[0], line[1]))
+        i += 1
 
-f.close()
+    f.close()
 
-col1 = []
-col2 = []
-col3 = []
-col4 = []
+    col1 = []
+    col2 = []
+    col3 = []
+    col4 = []
 
-for point in points:
-	if float(point[1]) < -2.5:
-		col1.append(point)
-	elif float(point[1]) < 0:
-		col2.append(point)
-	elif float(point[1]) < 2.5:
-		col3.append(point)
-	else:
-		col4.append(point)
+    for point in points:
+        if float(point[1]) < -2.5:
+            col1.append(point)
+        elif float(point[1]) < 0:
+            col2.append(point)
+        elif float(point[1]) < 2.5:
+            col3.append(point)
+        else:
+            col4.append(point)
 
-#Sort columns by number of points
+    #Sort columns by number of points
 
-cols = [(col, len(col)) for col in [col1, col2, col3, col4]]
+    cols = [(col, len(col)) for col in [col1, col2, col3, col4]]
 
-cols = sorted(cols, key = lambda x: x[1])
+    cols = sorted(cols, key = lambda x: x[1])
 
-for col in cols:
-	print col
-	col = sorted(col, key = lambda x: x[2])
+    cols.reverse()
 
-for i in range(len(cols)):
-	if i%2 == 0:
-		cols[i].reverse()
+    #Sort points within each column
+    cols = [sorted(col[0], key=lambda x: float(x[2])) for col in cols]
 
-points = [item for sublist in cols for item in sublist]
 
-f = open("points.txt", "w")
+    #Reverse every other column for back-and-forth
+    for i in range(len(cols)):
+        if (i+1)%2 == 0:
+            cols[i].reverse()
 
-f.write("SORT_THIS_SHIT:\n")
+    points = [point for col in cols for point in col]
 
-#Write list of X values
-for i in range(len(points)):
-	f.write("X%02d: DW %s\n" % (i+1, points[i][1]))
+    #print points
+    f = open("points_out.txt", "w")
 
-#Write list of Y values
-f.write("\n")
-for i in range(len(points)):
-	f.write("Y%02d: DW %s\n" % (i+1, points[i][1]))
+    f.write("SORT_THIS_SHIT:\n")
 
-f.write("\n")
-f.write("Points: DW X%02d\n" %(points[0][0],))
+    #Write list of X values
+    for i in range(len(points)):
+        f.write("X%02d: DW %s\n" % (i+1, points[i][1]))
 
-#Write list of point labels
-f.write("\n")
-for i in range(len(points)):
-	f.write("Pnt%02d: DW X%02d\n" %(i+1, points[i][0]))
+    #Write list of Y values
+    f.write("\n")
+    for i in range(len(points)):
+        f.write("Y%02d: DW %s\n" % (i+1, points[i][2]))
 
-f.close()
+    f.write("\n")
+    f.write("Points: DW X%02d\n" %(points[0][0],))
+
+    #Write list of point labels
+    f.write("\n")
+    for i in range(len(points)):
+        f.write("Pnt%02d: DW X%02d\n" %(i+1, points[i][0]))
+
+    f.close()
+
+if __name__ == "__main__":
+    sort()
